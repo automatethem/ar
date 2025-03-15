c3e04083c3432563844547ed89f2c5a6fefbaca5
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2025-03-16 00:32:31 +0900

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,3 +1,18 @@
 console.log("I am content-script.js");
 
-document.body.style.backgroundColor = 'blue';
+ function override_alert() {
+        window.alert = function alert(msg) {
+            console.log('Hidden Alert ' + msg);
+        };
+    }
+
+var script = document.createElement('script');
+script.appendChild(document.createTextNode('(' + override_alert + ')();'));
+document.documentElement.appendChild(script);
+
+
+
+// window.alert = function alert(msg) {
+//             console.log('Hidden Alert ' + msg);
+//         };
+// window.alert(2222)
